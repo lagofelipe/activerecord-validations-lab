c24bd34ea248b76fffd5c5d458b8b6de2a6eb37c
lagofelipe <lagofelipe@gmail.com>
2020-11-02 10:57:18 -0600

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ActiveRecord::Base
+    belongs_to :author
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :clickbait?
+
+
+def clickbait?
+    criteria = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top [0-9]*/i,
+        /Guess/i
+      ]
+
+      if criteria.none?{|phrase|phrase.match title}
+        errors.add(:title, "NOT clickbaity")
+
+end
+end
 end
